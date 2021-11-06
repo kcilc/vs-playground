a266d19b385b66444c782f9f880b8d3b7dd86d03
kcilc <ijscovos@gmail.com>
2021-11-05 21:27:51 -0500

    Found the bug

--- scripts.js
+++ scripts.js
@@ -1,9 +1,6 @@
-console.log('Hi!');
-
 function insertGreeting(phrase) {
-    console.log(phrase);
     let textual = document.getElementById('phrase-wrapper');
-    textual.innerText(phrase);
+    textual.innerText = phrase;
 }
 
-insertGreeting('We re live!');
+window.addEventListener('DOMContentLoaded', ()=> insertGreeting('We re live!'));
